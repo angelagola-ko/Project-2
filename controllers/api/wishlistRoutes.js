db79646f24962755b34c723a0a4816c4123f02d5
bortkyle18 <bortkyle18@gmail.com>
2022-07-08 17:50:52 -0400

    seed files

--- controllers/api/wishlistRoutes.js
+++ controllers/api/wishlistRoutes.js
@@ -1,10 +1,60 @@
 const router = require('express').Router();
 const { Wishlist, User } = require('../../models');
 const withAuth = require('../../utils/auth');
-const googleApi = 'AIzaSyDOISfO-M5SMbBSWWcZ615CngMyVBV0Cn4';
 
 
-router.post('/wishlist', withAuth, (req, res) => {
+// GET all wishlist locations for wishlist page
+router.get('/', async (req, res) => {
+    Wishlist.findAll({
+        attributes: [
+            'id',
+            'location',
+            'photo',
+            'user_id',
+        ]
+    })
+    .then(dbWishlistData => {
+        if (!dbWishlistData) {
+            res.status(404).json({ message: 'No Wishlist found' });
+            return;
+        }
+        res.json(dbWishlistData);
+        })
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+// GET one wishlist location by city
+router.get('/:city', async (req, res) => {
+    Wishlist.findOne({
+        where: {
+            city: req.params.city
+        },
+        attributes: [
+            'id',
+            'location',
+            'photo',
+            'user_id',
+        ]
+    })
+    .then(dbWishlistData => {
+        if (!dbWishlistData) {
+            res.status(404).json({ message: 'No Wishlist found for this city' });
+            return;
+        }
+        res.json(dbWishlistData);
+        })
+        .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+
+router.post('/', withAuth, (req, res) => {
     // Get photo
     cityLocation = req.body.location.replace(/ /g, '-').replace(/\./g, '').toLowerCase();
     console.log(cityLocation); 
