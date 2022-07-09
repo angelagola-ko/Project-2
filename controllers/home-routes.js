2b6cd897a7825c12eb0a15958cd3b6781d3b3d6d
bortkyle18 <bortkyle18@gmail.com>
2022-07-09 13:00:56 -0400

    wishlist handlebars page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -8,6 +8,10 @@ router.get("/trips" ,(req,res) => {
     res.render("trips");
 })
 
+router.get("/wishlist" , (req,res) => {
+    res.render("wishlist");
+})
+
 const { Wishlist, User, Trips } = require('../models');
 
 
