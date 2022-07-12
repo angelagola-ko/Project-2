50d56f69cf17eaacf0a649b7e204dcee9854dc7c
bortkyle18 <bortkyle18@gmail.com>
2022-07-12 01:45:24 -0400

    heroku test

--- controllers/wishlistroutes.js
+++ controllers/wishlistroutes.js
@@ -28,20 +28,44 @@ router.get("/" , (req,res) => {
 
 // Add city to wishlist page
 router.post('/', (req, res) => {
-    Wishlist.create({
-        location: req.body.location,
-        photo: req.body.photo,
-        // user_id: req.body.user_id
-    })
-    .then(
-    dbWishlistData => {
-    const wishlist = dbWishlistData.map(wishlist => wishlist.get({ plain: true }));
-    res.render('wishlist', { wishlist })
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+    // Get photo
+    var getCity = function (city) {
+        cityLocation = city.replace(/ /g, '-').replace(/\./g, '').toLowerCase();
+        console.log(cityLocation);
+
+        fetch(`https://api.teleport.org/api/urban_areas/slug:${cityLocation}/images/`)
+        .then(function (response) {
+            if (response.statusText != 'OK') {
+                makeWishlist(`https://static.turbosquid.com/Preview/001325/331/VU/_DHQ.jpg`)
+            } else {
+                response.json()
+                .then(function (data) {
+                    makeWishlist(data.photos[0].image.mobile);
+                });
+            }
+        })
+    }
+    // End of Get Photo
+
+    // Create wishlist object
+    var makeWishlist = function (cityPhoto) {
+        Wishlist.create({
+            location: req.body.location,
+            photo: cityPhoto,
+            // user_id: req.body.user_id
+        })
+        .then(
+        dbWishlistData => {
+        const wishlist = [dbWishlistData].map(wishlist => wishlist.get({ plain: true }));
+        res.render('wishlist', { wishlist })
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    };
+
+    getCity(req.body.location);
 });
 
 // go to single wishlist city location
--- public/assets/js/wishlist-post.js
+++ public/assets/js/wishlist-post.js
@@ -1,33 +1,12 @@
 async function newWishlist(event) {
     event.preventDefault();
 
-    // Get photo
-    var getCity = function (city) {
-        cityLocation = city.replace(/ /g, '-').replace(/\./g, '').toLowerCase();
-        console.log(cityLocation);
+    const location = document.querySelector('input[name="location"]').value;
 
-        fetch(`https://api.teleport.org/api/urban_areas/slug:${cityLocation}/images/`)
-        .then(function (response) {
-            if (response.statusText != 'OK') {
-                return `https://static.turbosquid.com/Preview/001325/331/VU/_DHQ.jpg`;
-            } else {
-                response.json()
-                .then(function (data) {
-                    return data.photos[0].image.mobile;
-                });
-            }
-        })
-    }
-    // End of Get Photo
-
-    const location = document.getElementById('location').value;
-    const photo = getCity(location)
-
-    await fetch(`/wishlist`, {
+    const response = await fetch(`/wishlist`, {
         method: 'POST',
         body: JSON.stringify({
             location,
-            photo,
             // body.session.user_id
         }),
         headers: {
